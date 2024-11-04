397fac350cf9a0e1333d1ee1a2949b70c1bc935a
OlegSadJktu <plyuha2004@mail.ru>
2024-11-04 23:49:57 +0600

    [Fix] Update generation

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 generate:
 	protoc --go_out=internal/generated --go_opt=paths=source_relative \
 	--go-grpc_out=internal/generated --go-grpc_opt=paths=source_relative \
-	--queque-ymq_out=internal/generated --queque-ymq_opt=paths=source_relative,msg=Msg \
+	--queque-ymq_out=internal/generated --queque-ymq_opt=paths=source_relative,msg=Msg+Popa \
 	proto/*/*.proto
 
--- cmd/protoc-gen-queque-ymq/main.go
+++ cmd/protoc-gen-queque-ymq/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"errors"
 	"flag"
+	"slices"
+	"strings"
 
 	"github.com/dr3dnought/queque_ymq_protoc_plugin/generator"
 	"google.golang.org/protobuf/compiler/protogen"
@@ -12,26 +14,33 @@ var (
 	ErrMessageNotDefined = errors.New(`message for generation not found`)
 )
 
-const messageSuffix = "QueQueMessage"
-
 func main() {
 	var flags flag.FlagSet
 	msgName := flags.String("msg", "", "message name")
 
 	protogen.Options{
-    ParamFunc: flags.Set,
-  }.Run(func(gen *protogen.Plugin) error {
+		ParamFunc: flags.Set,
+	}.Run(func(gen *protogen.Plugin) error {
+		// return errors.New(fmt.Sprintf("msg %v", *msgName))
+		if msgName == nil || *msgName == "" {
+			return ErrMessageNotDefined
+		}
+		msgs := strings.Split(*msgName, "+")
 		for _, f := range gen.Files {
 			if !f.Generate {
 				continue
 			}
-			if msgName == nil || *msgName == "" {
-				return ErrMessageNotDefined
+			for _, msg := range f.Messages {
+				goName := msg.GoIdent.GoName
+				if slices.Contains(msgs, goName) {
+					err := generateFile(gen, f, goName)
+					if err != nil {
+						return err
+					}
+
+				}
 			}
-			err := generateFile(gen, f, *msgName)
-			if err != nil {
-				return err
-			}
+			// if slices.Contains(msgs, )
 		}
 		return nil
 	})
